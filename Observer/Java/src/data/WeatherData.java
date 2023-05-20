9413b31364c75ccbf3734b4affb1807c5d9b274a
Szvetlin Tanyi <svetlintanyi@gmail.com>
2023-05-20 12:43:07 +0200

    Observer pull pattern

--- Observer/Java/src/data/WeatherData.java
+++ Observer/Java/src/data/WeatherData.java
@@ -29,7 +29,7 @@ public class WeatherData implements Subject {
     @Override
     public void notifyObservers(){
         for(Observer observer: observers){
-            observer.update(temperature, humidity, pressure);
+            observer.update();
         }
     }
 
@@ -42,5 +42,17 @@ public class WeatherData implements Subject {
         this.humidity = humidity;
         this.pressure = pressure;
         measurementsChanged();
+    }
+
+    public float getTemperature() {
+        return temperature;
+    }
+
+    public float getHumidity() {
+        return humidity;
+    }
+
+    public float getPressure() {
+        return pressure;
     }
 }
--- Observer/Java/src/interfaces/Observer.java
+++ Observer/Java/src/interfaces/Observer.java
@@ -1,5 +1,5 @@
 package eu.svetlin.design.patterns.observer.interfaces;
 
 public interface Observer {
-    public void update(float temperature, float humidity, float pressure);
+    public void update();
 }
