02285adcaa7a6f7db4fd59e4ddb3798405e7b7f1
Szvetlin Tanyi <svetlintanyi@gmail.com>
2023-08-04 16:04:27 +0200

    Facade pattern java

---
+++ Facade/Java/src/HomeTheaterFacade.java
@@ -0,0 +1,80 @@
+import devices.Amplifier;
+import devices.CdPlayer;
+import devices.PopcornPopper;
+import devices.Projector;
+import devices.Screen;
+import devices.StreamingPlayer;
+import devices.TheaterLights;
+import devices.Tuner;
+
+public class HomeTheaterFacade {
+	Amplifier amp;
+	Tuner tuner;
+	StreamingPlayer player;
+	CdPlayer cd;
+	Projector projector;
+	TheaterLights lights;
+	Screen screen;
+	PopcornPopper popper;
+
+	public HomeTheaterFacade(Amplifier amp,
+				 Tuner tuner,
+				 StreamingPlayer player,
+				 CdPlayer cd,
+				 Projector projector,
+				 Screen screen,
+				 TheaterLights lights,
+				 PopcornPopper popper) {
+
+		this.amp = amp;
+		this.tuner = tuner;
+		this.player = player;
+		this.cd = cd;
+		this.projector = projector;
+		this.screen = screen;
+		this.lights = lights;
+		this.popper = popper;
+	}
+
+	public void watchMovie(String movie) {
+		System.out.println("Get ready to watch a movie...");
+		popper.on();
+		popper.pop();
+		lights.dim(10);
+		screen.down();
+		projector.on();
+		projector.wideScreenMode();
+		amp.on();
+		amp.setStreamingPlayer(player);
+		amp.setSurroundSound();
+		amp.setVolume(5);
+		player.on();
+		player.play(movie);
+	}
+
+	public void endMovie() {
+		System.out.println("Shutting movie theater down...");
+		popper.off();
+		lights.on();
+		screen.up();
+		projector.off();
+		amp.off();
+		player.stop();
+		player.off();
+	}
+
+	public void listenToRadio(double frequency) {
+		System.out.println("Tuning in the airwaves...");
+		tuner.on();
+		tuner.setFrequency(frequency);
+		amp.on();
+		amp.setVolume(5);
+		amp.setTuner(tuner);
+	}
+
+	public void endRadio() {
+		System.out.println("Shutting down the tuner...");
+		tuner.off();
+		amp.off();
+	}
+}
