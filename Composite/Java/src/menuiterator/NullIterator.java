46fee2cc38cffc3da80ddf1d60434ef8118ea8d1
Szvetlin Tanyi <svetlintanyi@gmail.com>
2023-11-19 21:01:08 +0100

    Composite Pattern

---
+++ Composite/Java/src/menuiterator/NullIterator.java
@@ -0,0 +1,14 @@
+package menuiterator;
+
+import java.util.Iterator;
+
+public class NullIterator implements Iterator<MenuComponent> {
+
+	public MenuComponent next() {
+		return null;
+	}
+
+	public boolean hasNext() {
+		return false;
+	}
+}
